e61b086aed40a32dbff0db4004d7891ad31ae509
ksauraj <gitsauraj@gmail.com>
2022-05-23 11:37:51 +0000

    root_files: full_T99651-user-12-SP1A.210812.016-00WW_3_210-release-keys-Wed-Apr-20-11-53-56-UTC-2022

---
+++ BoardConfig.mk
@@ -0,0 +1,30 @@
+DEVICE_PATH := device/nokia/Wolverine
+BOARD_VENDOR := nokia
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2022-04-05
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.biometrics.fingerprint@2.1-service.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.boot@1.2.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.cas@1.2-service-lazy.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.health@2.1.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.usb@1.2-service-mediatekv2.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.wifi.hostapd.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.wifi.supplicant.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.wifi@1.0-service.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/gnss-default.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/gnss@2.1-service.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/lbs_hidl_service@1.0.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/lights-mtk-default.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest_android.hardware.drm@1.4-service.clearkey.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest_android.hardware.drm@1.4-service.widevine.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest_hwcomposer.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest_media_c2_V1_2_default.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/power-default.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vibrator-mtk-default.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
+-include vendor/nokia/Wolverine/BoardConfigVendor.mk
